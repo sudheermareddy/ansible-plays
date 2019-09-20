51695a3a0c9bec62022f8d393943e51c6c084c09
sushmithan <sushmithan@sysgain.com>
2019-09-20 16:38:40 +0530

    Create ExtractUploadUserTLData.sh

---
+++ ExtractUploadUserTLData.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+STORAGE_ACCOUNT="tlnprreports"
+DB_NAME="tl_veegam_user_training"
+TABLE_NAME="user_training"
+CONTAINER_NAME="tl-reports/user-training"
+SASTOKEN="sv=2018-03-28&ss=bfqt&srt=sco&sp=rwdlacup&se=2025-09-17T06:11:42Z&st=2019-09-09T22:11:42Z&spr=https,http&sig=MEuIOdVgQ0nutZtQDsnHH%2BOeGAbq7BEACS3aa9JE%2FBI%3D"
+HOST="rdbokenprtl"
+PORT="28015"
+if [ -n "$STORAGE_ACCOUNT" ] && [ -n "$TABLE_NAME" ] && [ -n "$DB_NAME" ] && [ -n "$CONTAINER_NAME" ] && [ -n "$SASTOKEN" ]
+then
+        fileNameExt=`date +%Y_%m_%d_%H_%M_%S`
+        fileName=$TABLE_NAME"_"$fileNameExt".json"
+        TIME_TYPE="hours"
+        TIME_DIFFERENCE=4
+        CURRENT_DATE=`date +%Y,%m,%d,%H,%M,%S`
+        PREVIOUS_DATE=`date --date "$TIME_DIFFERENCE $TIME_TYPE ago" +%Y,%m,%d,%H,%M,%S`
+        echo "Export for data started"
+        echo "r.db('$DB_NAME').table('$TABLE_NAME').filter(r.row('launchTime').during(r.time($PREVIOUS_DATE,'Z'), r.time($CURRENT_DATE,'Z')))"
+        /usr/local/bin/recli -h $HOST -p $PORT --json "r.db('$DB_NAME').table('$TABLE_NAME').hasFields('trainingRequestedTimestamp','launchTime').filter(r.row('launchTime').during(r.time($PREVIOUS_DATE,'Z'), r.time($CURRENT_DATE,'Z'))).pluck('launchState','trainingRequestedTimestamp','user_training_id','orgId','userEmail','userId','userName','modeType',{'finalResponse':{'status': true}},'transfer_group','trainingDeploymentId','launchTime','cleanupStatus','channelData','publisherOrg','isEmailSent','labId','labName').map(function(row){return row.merge({'trainingRequestedTimestamp' : row('trainingRequestedTimestamp').date().toISO8601().slice(0,-6),'launchTime' : row('launchTime').date().toISO8601().slice(0,-6)})})" > $fileName
+        echo "Export finished and Upload to container started"
+        curl -X PUT -T $fileName -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "https://$STORAGE_ACCOUNT.blob.core.windows.net/$CONTAINER_NAME/$fileName?$SASTOKEN"
+        echo "Upload finished"
+#        echo "Az function import started"
+#        curl -X GET "$FUNCTION_URL&fileName=$fileName"
+#        echo "Az import finished"
+else
+        echo -e "required ENV variables not set.\n\n required variables: \n\tSTORAGE_ACCOUNT\n\tTABLE_NAME\n\tDB_NAME\n\tCONTAINER_NAME\n\tSASTOKEN"
+fi
