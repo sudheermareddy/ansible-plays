dcd14a7557763ab0bc61180ac9d26f6c19435c71
sudheermareddy <sudheerm@sysgain.com>
2020-11-26 16:54:44 +0530

    Create oke-npr-rdb-db-check.sh

---
+++ ansible/playbooks/scripts/oke-npr-rdb-db-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+data_path="/data01/var/lib/rethinkdb/data"
+recli "r.dbList()" > $data_path/dblist_current
+if diff $data_path/dblist $data_path/dblist_current > /dev/null
+then
+ diff $data_path/dblist $data_path/dblist_current
+echo "no changes in DB"
+else
+echo "Error : DB has below changes"
+diff $data_path/dblist $data_path/dblist_current
+fi
