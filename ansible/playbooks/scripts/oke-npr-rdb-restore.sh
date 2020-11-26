7137a7536a9c31ea0076624063e00547c42f6cdf
sudheermareddy <sudheerm@sysgain.com>
2020-11-26 14:10:10 +0530

    Update oke-npr-rdb-restore.sh

--- ansible/playbooks/scripts/oke-npr-rdb-restore.sh
+++ ansible/playbooks/scripts/oke-npr-rdb-restore.sh
@@ -4,19 +4,24 @@ replica=$3
 data_path="/data01/var/lib/rethinkdb/data"
 
 #list dbs
-recli "r.dbList()" | tr -d ,[]' '|sed $'s/[^[:print:]\t]//g'| sed 's/32m//g'| sed 's/39m//g'|sed '/'rethinkdb'/d' > /tmp/dblist
-#drop existing dbs 
-if cat /tmp/dblist | wc -l > 0; then
+recli "r.dbList()" | tr -d ,[]|sed $'s/[^[:print:]\t]//g'| sed 's/32m//g'| sed 's/39m//g'|sed '/'rethinkdb'/d' | tr -dc '[:print:]'|sed 's/\s\+/\n/g'|awk 'NF' > /tmp/dblist
+#recli "r.dbList()" | tr -d ,[]' '|sed $'s/[^[:print:]\t]//g'| sed 's/32m//g'| sed 's/39m//g'|sed '/'rethinkdb'/d' |  tr -dc '[:alnum:]\n\r' > /tmp/dblist
+#drop existing dbs
+if cat /tmp/dblist | wc -l < 0; then
  while read p
 do
 recli "r.dbDrop($p)"
 done < /tmp/dblist
  fi
 #removing old data
+#file=($data_path/*.tar.gz*)
+# if [ -e "${file[0]}" ]; then
  rm $data_path/*.tar.gz*
+ #fi
 if [ -d "$data_path/tmpfiles" ]; then
 sudo rm -rf $data_path/tmpfiles
 fi
+
 #download and un tar the rdb backup files
 cd $data_path
 wget $url
@@ -24,21 +29,14 @@ tar xvf *.tar.gz*
 #restoring the rdb databases with shard and repica count.
 if [ -d "$data_path/tmpfiles" ]; then
         ls $data_path/tmpfiles > /tmp/bkplist
+    cd  $data_path/tmpfiles
 while read p
 do
 echo $p
 q=$(echo $p | awk -F '\\_Bak.tar' '{print $1"_Bak.tar"}' | sed 's/_Bak.tar//')
-rethinkdb restore $p -i $q --shards $shared --replicas $replica
+rethinkdb restore $p -i $q --shards $shared --replicas $replica --force
 echo $q
 echo "$p db is restored"
 done < /tmp/bkplist
 fi
-
-
-#a=$1
-#b=$2
-#c=$3
-
-#echo "$a and $b and $c"
-#touch /tmp/$a /tmp/$b /tmp/$c
-
+rm /tmp/bkplist /tmp/dblist
